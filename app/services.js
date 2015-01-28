9c683a86be1699557e23679e7f882da08e8ad572
Ahmed H. Ismail <ahm3d.hisham@gmail.com>
2015-01-28 16:13:29 +0200

    Removes debugger breakpoint

--- app/services.js
+++ app/services.js
@@ -40,7 +40,6 @@ jprServices.factory('Page', ['$rootScope', 'Host', '$http', function($rootScope,
         var fileName = match[1];
         var contentType = config('Content-Type');
         if (contentType != 'text/plain') {
-          debugger
           var bytes = new Uint8Array(data.length);
           for(var i=0; i < data.length; i++) {
             bytes[i] = data.charCodeAt(i);
