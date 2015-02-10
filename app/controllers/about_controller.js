ba628e62e8600a9e9c10748e8dae7f4a55073674
Ahmed H. Ismail <ahm3d.hisham@gmail.com>
2015-02-10 15:51:20 +0200

    Creates about page

--- app/controllers/about_controller.js
+++ app/controllers/about_controller.js
@@ -1,4 +1,4 @@
-jprApp.controller('AboutCtrl', ['$scope', 'Page', function($scope, page){
+jprApp.controller('AboutCtrl', ['$scope', 'Page', function($scope, Page){
     Page.setSection('About');
     Page.setLink('about');
 
