83b18e91676dd37f7a2b76d487475f0fba21ca69
Ahmed H. Ismail <ahm3d.hisham@gmail.com>
2016-01-01 03:22:46 +0200

    Adds SubjectTagInterceptor to email previews

--- config/initializers/mailer_opts.rb
+++ config/initializers/mailer_opts.rb
@@ -2,10 +2,14 @@ Rails.configuration.action_mailer.default_url_options = {
   host: 'evaluator.in'
 }
 
-class EmailSubjectTag
-  def self.delivering_email(mail)
-    mail.subject = '[EVALUATOR] ' + mail.subject
+class SubjectTaggerEmailInterceptor
+  def self.delivering_email(message)
+    message.subject = '[EVALUATOR] ' + message.subject
+  end
+  def self.previewing_email(message)
+    SubjectTaggerEmailInterceptor.delivering_email message
   end
 end
 
-ActionMailer::Base.register_interceptor EmailSubjectTag
+ActionMailer::Base.register_interceptor SubjectTaggerEmailInterceptor
+ActionMailer::Base.register_preview_interceptor SubjectTaggerEmailInterceptor
