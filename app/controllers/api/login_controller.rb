65dc3b1f8917d81546bf4ed09323384c3ed561ab
Ahmed H. Ismail <ahm3d.hisham@gmail.com>
2016-01-29 18:06:04 +0200

    creates login controller

---
+++ app/controllers/api/login_controller.rb
@@ -0,0 +1,21 @@
+class Api::LoginController < ApplicationController
+  skip_filter :authorize, :authenticate, only: [:create]
+  
+  def create
+    user = User.find_by_email(login_params[:email])
+    raise AuthenticationError if user.nil?
+    raise AuthenticationError unless user.authenticate(login_params[:password])
+    raise ForbiddenError, error_messages[:unverified_login] unless user.verified?
+    render json: {
+      token: user.token(login_params[:expiration]),
+      user: user
+    }, status: :created
+  end
+
+  private
+
+  def login_params
+    params.require(:token).permit([:email, :password, :expiration])
+  end
+
+end
