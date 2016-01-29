6bf0e32569817a94533d600f0e3fa02c3dbfa0cb
Ahmed H. Ismail <ahm3d.hisham@gmail.com>
2016-01-29 17:14:45 +0200

    Removes expires header value

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -74,7 +74,6 @@ class ApplicationController < ActionController::API
   def no_cache
     response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate" # HTTP 1.1.
     response.headers["Pragma"] = "no-cache" # HTTP 1.0.
-    response.headers["Expires"] = "0" # Proxies.
   end
 
   # The resource class based on resource_name
