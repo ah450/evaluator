740f5af733e3f7d8550ee9907a44214070743bb2
Ahmed H. Ismail <ahm3d.hisham@gmail.com>
2016-02-07 02:37:35 +0200

    Fixes #14.
    Signed-off-by: Ahmed H. Ismail <ahm3d.hisham@gmail.com>

--- app/jobs/destroy_test_suite_job.rb
+++ app/jobs/destroy_test_suite_job.rb
@@ -2,11 +2,14 @@ class DestroyTestSuiteJob < ActiveJob::Base
   queue_as :default
 
   def perform(test_suite)
+    # TODO: Change to destroy results only
     project = test_suite.project
     project.with_lock('FOR UPDATE') do
       # submissions with a result belonging to this suite
       Submission.where(project: project).joins(:results).where(results:
-        {test_suite_id: test_suite.id}).destroy_all
+        {test_suite_id: test_suite.id}).each do |submission|
+          submission.results.destroy_all
+        end
       test_suite.destroy
     end
   end
