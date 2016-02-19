f4c5e224fe7c7f593308972c8f5be3a6b111e6c0
Ahmed H. Ismail <ahm3d.hisham@gmail.com>
2016-02-19 19:43:59 +0200

    Style fixes for tokens controller and related specs
    Signed-off-by: Ahmed H. Ismail <ahm3d.hisham@gmail.com>

--- app/controllers/api/tokens_controller.rb
+++ app/controllers/api/tokens_controller.rb
@@ -1,11 +1,9 @@
 class Api::TokensController < ApplicationController
-  skip_filter :authorize, :authenticate, only: [:create]
-  
+  skip_action_callback :authorize, :authenticate, only: [:create]
+
   def create
     user = User.find_by_email(token_params[:email].downcase)
-    raise AuthenticationError if user.nil?
-    raise AuthenticationError unless user.authenticate(token_params[:password])
-    raise ForbiddenError, error_messages[:unverified_login] unless user.verified?
+    authenticate_user(user)
     render json: {
       token: user.token(token_params[:expiration]),
       user: user
@@ -13,6 +11,15 @@ class Api::TokensController < ApplicationController
   end
 
   private
+
+  def authenticate_user(user)
+    raise AuthenticationError if user.nil?
+    raise AuthenticationError unless user.authenticate(token_params[:password])
+    raise ForbiddenError, error_messages[:unverified_login] unless
+      user.verified?
+  end
+
+
   def token_params
     params.require(:token).permit([:email, :password, :expiration])
   end
