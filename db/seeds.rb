4420bfe12ee54b24b6ce903f5e2329c84afca996
Ahmed H. Ismail <ahm3d.hisham@gmail.com>
2016-02-22 05:00:11 +0200

    Adds seed data for quick testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+course = FactoryGirl.create(:course, published: true)
+project = FactoryGirl.create(:project, course: course, published: true)
+submissions = FactoryGirl.create_list(:submission_with_code, 10, project: project)
