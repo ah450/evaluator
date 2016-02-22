729a226867dee3a08928f92e590c45fe13d14080
Ahmed H. Ismail <ahm3d.hisham@gmail.com>
2016-02-22 05:55:05 +0200

    Adds rescue from for destroy_project bundle job
    Signed-off-by: Ahmed H. Ismail <ahm3d.hisham@gmail.com>

--- app/jobs/destroy_project_bundle_job.rb
+++ app/jobs/destroy_project_bundle_job.rb
@@ -1,6 +1,9 @@
 class DestroyProjectBundleJob < ActiveJob::Base
   queue_as :default
 
+  rescue_from(ActiveJob::DeserializationError) do
+  end
+
   def perform(project_bundle)
     project_bundle.destroy
   end
