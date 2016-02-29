182784b787f12c49e698730425837ca8d55f5a3a
Ahmed H. Ismail <ahm3d.hisham@gmail.com>
2016-02-29 19:10:59 +0200

    Faye initialization for testing environment
    Signed-off-by: Ahmed H. Ismail <ahm3d.hisham@gmail.com>

--- config/initializers/faye.rb
+++ config/initializers/faye.rb
@@ -1,12 +1,11 @@
 # Faye configurations
 Rails.application.config.middleware.delete Rack::Lock
 
+Faye.logger = Rails.logger
 unless Rails.const_defined?(:Server) || !ENV['IS_SERVER'].nil?
-  Faye.logger = Rails.logger
   Faye.logger.debug 'Ensure reactor running!'
   Faye.ensure_reactor_running!
 end
-
 Rails.application.config.middleware.use FayeRails::Middleware, mount: '/faye', engine: {
   type: Faye::Redis,
   host: 'localhost'
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -11,6 +11,8 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort('The Rails environment is running in production mode!') if Rails.env.production?
+Faye.ensure_reactor_running!
+
 require 'spec_helper'
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
